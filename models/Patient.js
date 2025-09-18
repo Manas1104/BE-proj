228d0e96ae11b86b9d1f63df5c58ae2898459c01
Krish Makwana <coolcoder29@gmail.com>
2025-09-18 15:14:51 +0530

    Remove all files from repo

--- models/Patient.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const patientSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    medicalHistory: { type: String, default: '' },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Patient', patientSchema);
--- routes/doctorRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getDoctorById } = require("../controllers/doctorController");
-
-const router = express.Router();
-
-// GET /api/doctors/:id → fetch doctor profile
-router.get("/:id", getDoctorById);
-
-module.exports = router;
